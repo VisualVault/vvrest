29ca3eb508c07f74d7a835fb0d8a9f8f0233305d
jaredrunyon <rootRaider@outlook.com>
2018-12-29 16:32:50 -0700

    add form instance unittests for FormService.

--- tests/form_service_test.py
+++ tests/form_service_test.py
@@ -13,6 +13,8 @@ class FormServiceTest(unittest.TestCase):
         cls.form_instance_name = 'test_tem-000001'
         cls.form_template_field_name = 'test_field'
         cls.form_instance_field_value = 'test_field value'
+        cls.document_revision_id = '8fefa9b6-56fa-e811-a995-a3d452a1c2f6'
+        cls.document_id = '810013e6-50fa-e811-a9cf-8b72d90dd505'
 
     def test_get_form_templates(self):
         """
@@ -166,3 +168,57 @@ class FormServiceTest(unittest.TestCase):
         self.assertEqual(form_instance['instanceName'], instance_name)
         self.assertIn(self.form_template_field_name, form_instance)
         self.assertEqual(form_instance[self.form_template_field_name], expected_field_value)
+
+    def test_relate_form(self):
+        """
+        tests the following FormService methods:
+        relate_form, get_related_forms, unrelate_form
+        """
+        form_service = FormService(self.vault)
+        payload = dict(test_field=generate_random_uuid())
+
+        resp = form_service.create_form_instance(self.form_template_id, payload)
+        self.assertEqual(resp['meta']['status'], 201)
+        self.assertEqual(resp['data']['dataType'], 'FormInstance')
+        instance_id2 = resp['data']['revisionId']
+
+        resp = form_service.relate_form(self.form_instance_id, instance_id2)
+        self.assertEqual(resp['meta']['status'], 200)
+
+        resp = form_service.get_related_forms(self.form_instance_id)
+        self.assertEqual(resp['meta']['status'], 200)
+        self.assertEqual(len(resp['data']), 1)
+        self.assertEqual(resp['data'][0]['dataType'], 'FormInstance')
+        self.assertEqual(resp['data'][0]['revisionId'], instance_id2)
+
+        resp = form_service.unrelate_form(self.form_instance_id, instance_id2)
+        self.assertEqual(resp['meta']['status'], 200)
+
+        resp = form_service.get_related_forms(self.form_instance_id)
+        self.assertEqual(resp['meta']['status'], 200)
+        self.assertEqual(len(resp['data']), 0)
+
+    def test_relate_document(self):
+        """
+        tests the following FormService methods:
+        relate_document, get_related_documents, and unrelate_document
+        """
+        # TODO: report return list of object instead of object on FormInstance, and FormTemplate endpoints
+        # TODO: report 404 if relationship already exists
+        form_service = FormService(self.vault)
+
+        resp = form_service.relate_document(self.form_instance_id, self.document_revision_id)
+        self.assertEqual(resp['meta']['status'], 200)
+
+        resp = form_service.get_related_documents(self.form_instance_id)
+        self.assertEqual(resp['meta']['status'], 200)
+        self.assertEqual(len(resp['data']), 1)
+        self.assertEqual(resp['data'][0]['dataType'], 'Document')
+        self.assertEqual(resp['data'][0]['documentId'], self.document_id)
+
+        resp = form_service.unrelate_document(self.form_instance_id, self.document_revision_id)
+        self.assertEqual(resp['meta']['status'], 200)
+
+        resp = form_service.get_related_documents(self.form_instance_id)
+        self.assertEqual(resp['meta']['status'], 200)
+        self.assertEqual(len(resp['data']), 0)
