0af556785c5d5a4fc63d1b49f5ca1a5ae43e9b54
rootRaider <rootRaider@outlook.com>
2021-03-09 10:43:00 -0700

    use bool data type for check_in param for file_upload

--- tests/file_service_test.py
+++ tests/file_service_test.py
@@ -79,7 +79,7 @@ class FileServiceTest(unittest.TestCase):
         file_service = FileService(self.vault)
         resp = file_service.file_upload(self.document_id, 'unittest', expected_revision, 'unittest change reason',
                                         'Released', '', 'unittest.txt', self.file_path + '/' + self.file_upload_name,
-                                        check_in=0)
+                                        check_in=True)
 
         self.assertEqual(resp['meta']['status'], 200)
         self.assertEqual(resp['data']['documentId'], self.document_id)
@@ -97,7 +97,7 @@ class FileServiceTest(unittest.TestCase):
         file_service = FileService(self.vault)
         resp = file_service.file_upload(self.document_id, 'unittest', expected_revision, 'unittest change reason',
                                         'Released', '', 'unittest.txt', self.file_path + '/' + self.file_upload_name,
-                                        check_in=1)
+                                        check_in=False)
 
         self.assertEqual(resp['meta']['status'], 200)
         self.assertEqual(resp['data']['documentId'], self.document_id)
